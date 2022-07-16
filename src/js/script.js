2501f1ff36ce720e8297af22e4bf9e0feace6609
Maksym <mak7pan@gmail.com>
2022-07-16 11:17:36 +0300

    added adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,3 @@
 $(function(){
-    var visi = [true, true, true, true, true];
     
-    $('.first__img').each(function(i, item){
-        console.log(i);
-        $(this).on('click', function(){
-            $('.first__descr').fadeOut(1000);
-        });
-    });
 });
