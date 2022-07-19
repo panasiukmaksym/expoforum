07c2320859e89cce9cf803c130302ea4db16ba3a
Maksym <mak7pan@gmail.com>
2022-07-19 23:00:04 +0300

    added phone menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,32 @@
 $(function(){
-    
+
+    $('.overflou__close').click(function(){
+        $('.overflou').fadeOut(500);
+    });
+
+    $('.header__menu').click(function(){
+        $('.overflou').fadeIn(500);
+    });
+
+    $('body').click(function(e){
+        if ( e.target.className == 'overflou' ) {
+            $('.overflou').fadeOut(500);
+        }
+    });
+
+    $('.first__card .first__arrow').each(function(item){
+        $(this).click(function(){
+            $($('.first__descr')[item]).slideToggle(1000);
+        });
+    });
+
+    $('.first__card').each(function(){
+        $(this).click(function(){
+            if ( $(this).css('background-color') !== 'rgb(0, 0, 0)' ) {
+                $(this).css('background', 'black');
+            } else {
+                $(this).css('background', 'orange');
+            }
+        });
+    });
 });
